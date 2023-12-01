9cf84378a2e655f197acd575f9ee7e23d8f37f7d
mor.verbin <mor.verbin@NLLD-2560>
2023-12-01 13:22:39 +0100

    Add project files.

---
+++ AdventOfCode/BaseTestableDay.cs
@@ -0,0 +1,21 @@
+﻿namespace AdventOfCode
+{
+    public enum RunMode
+    {
+        Test,
+        Real,
+    }
+
+    public abstract class BaseTestableDay: BaseDay
+    {
+        internal RunMode RunMode { get; set; }
+
+        protected override string InputFileExtension =>
+            RunMode switch
+            {
+                RunMode.Test => "test.txt",
+                RunMode.Real => ".txt",
+                _ => throw new ArgumentException("Invalid enum value for RunMode", nameof(RunMode)),
+            };
+    }
+}
---
+++ AdventOfCode/Day01.cs
@@ -0,0 +1,57 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("AdventOfCode.Tests")]
+namespace AdventOfCode
+{
+    public sealed class Day01 : BaseTestableDay
+    {
+        private readonly List<int> _input;
+
+        public Day01() : this(RunMode.Real)
+        {
+        }
+
+        public Day01(RunMode runMode)
+        {
+            RunMode = runMode;
+
+            _input = File
+                .ReadAllLines(InputFilePath)
+                .Select(int.Parse)
+                .ToList();
+        }
+
+        internal static int CalculateSingleFuel(int mass)
+        {
+            return (int)Math.Floor(mass / 3.0) - 2;
+        }
+
+        internal static int CalculateSingleCompoundFuel(int mass)
+        {
+            int extraFuel = Math.Max(0, CalculateSingleFuel(mass));
+            int totalFuel = extraFuel;
+
+            while (extraFuel > 0)
+            {
+                extraFuel = Math.Max(0, CalculateSingleFuel(extraFuel));
+                totalFuel += extraFuel;
+            }
+
+            return totalFuel;
+        }
+
+        private int CalculateFuel()
+        {
+            return _input.Select(CalculateSingleFuel).Sum();
+        }
+
+        private int CalculateCompoundFuel()
+        {
+            return _input.Select(CalculateSingleCompoundFuel).Sum();
+        }
+
+        public override ValueTask<string> Solve_1() => new($"{CalculateFuel()}");
+
+        public override ValueTask<string> Solve_2() => new($"{CalculateCompoundFuel()}");
+    }
+}
