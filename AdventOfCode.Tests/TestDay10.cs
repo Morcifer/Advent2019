10303bfb984881baf0150bd7e57fdc96b0086290
Morcifer <mor.verbin@gmail.com>
2024-02-27 14:58:49 +0100

    Day 10: part 2. Could really use a cleanup, but I'm not gonna.

--- AdventOfCode.Tests/TestDay10.cs
+++ AdventOfCode.Tests/TestDay10.cs
@@ -3,8 +3,8 @@ namespace AdventOfCode.Tests;
 public class TestDay10
 {
     [Theory]
-    [InlineData(typeof(Day10), RunMode.Test, "41", "-1")]
-    [InlineData(typeof(Day10), RunMode.Real, "326", "-1")]
+    [InlineData(typeof(Day10), RunMode.Test, "210", "802")]
+    [InlineData(typeof(Day10), RunMode.Real, "326", "1623")]
     public async Task Day10_Regression(Type type, RunMode runMode, string expectedPart1, string expectedPart2)
     {
         if (Activator.CreateInstance(type, runMode) is BaseTestableDay instance)
--- AdventOfCode/Day10.cs
+++ AdventOfCode/Day10.cs
@@ -4,6 +4,8 @@ namespace AdventOfCode;
 
 public sealed class Day10 : BaseTestableDay
 {
+    // TODO 1: use a sparse representation, even though it looks like it's unnecessary.
+    // TODO 2: Introduce grid coordinate struct that has (x, y) and (row, column), because this is silly!
     private readonly List<string> _input;
 
     public Day10() : this(RunMode.Real)
@@ -14,67 +16,113 @@ public sealed class Day10 : BaseTestableDay
     {
         RunMode = runMode;
 
-        // We can probably do better by using a sparse representation, but it looks like it's unnecessary.
         _input = File
             .ReadAllLines(InputFilePath)
             .ToList();
     }
 
-    private int VisibleAsteroids(int monitoringRow, int monitoringColumn)
+    private Dictionary<(int DY, int DX), List<(int DY, int DX)>> AsteroidsPerLineOfSight(int monitoringY, int monitoringX)
     {
-        var directions = new HashSet<(int, int)>();
+        var result = new Dictionary<(int DY, int DX), List<(int DY, int DX)>>();
 
-        for (var rowIndex = 0; rowIndex < _input.Count; rowIndex++)
+        for (var y = 0; y < _input.Count; y++)
         {
-            for (var columnIndex = 0; columnIndex < _input[0].Length; columnIndex++)
+            for (var x = 0; x < _input[0].Length; x++)
             {
-                if (_input[rowIndex][columnIndex] != '#')
+                if (_input[y][x] != '#')
                 {
                     continue;
                 }
 
-                var deltaRow = monitoringRow - rowIndex;
-                var deltaColumn = monitoringColumn - columnIndex;
+                var deltaY = y - monitoringY;
+                var deltaX = x - monitoringX;
 
-                var greatestCommonDivisor = Euclid.GreatestCommonDivisor(deltaRow, deltaColumn);
+                var greatestCommonDivisor = Euclid.GreatestCommonDivisor(deltaY, deltaX);
 
-                if (greatestCommonDivisor == 0)
+                var key = greatestCommonDivisor == 0
+                    ? (deltaY, deltaX)
+                    : ((int)(deltaY / greatestCommonDivisor), (int)(deltaX / greatestCommonDivisor));
+
+                if (!result.ContainsKey(key))
                 {
-                    directions.Add((deltaRow, deltaColumn));
+                    result[key] = new List<(int, int)>();
                 }
-                else
-                {
-                    var key = ((int)(deltaRow / greatestCommonDivisor), (int)(deltaColumn / greatestCommonDivisor));
-                    directions.Add(key);
-                }
+
+                result[key].Add((deltaY, deltaX));
             }
         }
 
-        return directions.Count - 1;
+        return result;
+    }
+
+    private ((int Y, int X) Location, Dictionary<(int DY, int DX), List<(int DY, int DX)>> Dictionary) BestStationLocation()
+    {
+        var allResults = new List<(int Visible, (int Y, int X) Location, Dictionary<(int DY, int DX), List<(int DY, int DX)>> Result)>();
+
+        for (var y = 0; y < _input.Count; y++)
+        {
+            for (var x = 0; x < _input[0].Length; x++)
+            {
+                if (_input[y][x] != '#')
+                {
+                    continue;
+                }
+
+                var result = AsteroidsPerLineOfSight(y, x);
+                allResults.Add((result.Keys.Count - 1, (y, x), result));
+            }
+        }
+
+        var best = allResults.MaxBy(x => x.Visible);
+
+        return (best.Location, best.Result);
     }
 
     private Answer CalculatePart1Answer()
     {
-        var max = 0;
-
-        for (var rowIndex = 0; rowIndex < _input.Count; rowIndex++)
-        {
-            for (var columnIndex = 0; columnIndex < _input[0].Length; columnIndex++)
-            {
-                if (_input[rowIndex][columnIndex] != '#')
-                {
-                    continue;
-                }
-
-                max = Math.Max(max, VisibleAsteroids(rowIndex, columnIndex));
-            }
-        }
-
-        return max;
+        var best = BestStationLocation();
+        return best.Dictionary.Count - 1;
     }
 
     private Answer CalculatePart2Answer()
     {
+        var (bestLocation, bestDictionary) = BestStationLocation();
+        bestDictionary.Remove((0, 0)); // Don't cut the branch you're sitting on.
+
+        var byDirection = bestDictionary
+            .OrderByDescending(kvp => kvp.Key.DX >= 0) // Right side, then left side
+            .ThenBy(kvp => Math.Atan2((double)kvp.Key.DX, -kvp.Key.DY))
+            .Select(kvp => kvp.Value
+                .OrderBy(x => Math.Abs(x.DX) + Math.Abs(x.DY))
+                .Select(x => (X: x.DX + bestLocation.X, Y: x.DY + bestLocation.Y))
+                .ToList()
+            )
+            .ToList();
+
+        var spaceLaseredDirectionIndex = 0;
+
+        for (var laseredCounter = 0; laseredCounter < 200; laseredCounter++)
+        {
+            var spaceLasered = byDirection[spaceLaseredDirectionIndex].First();
+
+            //Console.WriteLine($"{laseredCounter + 1}: ({spaceLasered.X}, {spaceLasered.Y}) has been space-lasered!!");
+
+            byDirection[spaceLaseredDirectionIndex].RemoveAt(0);
+
+            if (!byDirection[spaceLaseredDirectionIndex].Any())
+            {
+                byDirection.RemoveAt(spaceLaseredDirectionIndex);
+                spaceLaseredDirectionIndex--;
+            }
+
+            if (laseredCounter == 199)
+            {
+                return spaceLasered.X * 100 + spaceLasered.Y;
+            }
+
+            spaceLaseredDirectionIndex = (spaceLaseredDirectionIndex + 1) % byDirection.Count;
+        }
+
         return -1;
     }
 
