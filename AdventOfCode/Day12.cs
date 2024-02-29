d87b5e22ab1b51bdfb72d5820dee3ba407e9b3b6
Morcifer <mor.verbin@gmail.com>
2024-02-29 09:15:46 +0100

    Day 12: Switch from tuple to list so that I can more easily do things for part 2.

--- AdventOfCode/Day12.cs
+++ AdventOfCode/Day12.cs
@@ -1,13 +1,11 @@
 ﻿using MoreLinq;
-using Vector3D = (int X, int Y, int Z);
+using Vector3D = System.Collections.Generic.List<int>;
 
 namespace AdventOfCode;
 
 public sealed class Day12 : BaseTestableDay
 {
-    // TODO 1: use a sparse representation, even though it looks like it's unnecessary.
-    // TODO 2: Introduce grid coordinate struct that has (x, y) and (row, column), because this is silly!
-    private readonly List<Vector3D> _input;
+   private readonly List<Vector3D> _input;
 
     public Day12() : this(RunMode.Real)
     {
@@ -22,31 +20,37 @@ public sealed class Day12 : BaseTestableDay
         switch (runMode)
         {
             case RunMode.Test:
-                _input.Add(new Vector3D(-1, 0, 2));
-                _input.Add(new Vector3D(2, -10, -7));
-                _input.Add(new Vector3D(4, -8, 8));
-                _input.Add(new Vector3D(3, 5, -1));
+                _input.Add([-1, 0, 2]);
+                _input.Add([2, -10, -7]);
+                _input.Add([4, -8, 8]);
+                _input.Add([3, 5, -1]);
+
+                //_input.Add(new Vector3D[-8, -10, 0]);
+                //_input.Add(new Vector3D[5, 5, 10]);
+                //_input.Add(new Vector3D[2, -7, 3]);
+                //_input.Add(new Vector3D[9, -8, -3]);
                 break;
 
             case RunMode.Real:
-                _input.Add(new Vector3D(-3, 15, -11));
-                _input.Add(new Vector3D(3, 13, -19));
-                _input.Add(new Vector3D(-13, 18, -2));
-                _input.Add(new Vector3D(6, 0, -1));
+                _input.Add([-3, 15, -11]);
+                _input.Add([3, 13, -19]);
+                _input.Add([-13, 18, -2]);
+                _input.Add([6, 0, -1]);
                 break;
         }
     }
 
-    private Answer SimulateGravity(int numberOfSteps)
+    private Answer SimulateGravity(int numberOfSteps, bool firstPart)
     {
         var positions = _input.ToList();
-        var velocities = _input.Select(_ => new Vector3D(0, 0, 0)).ToList();
+        var velocities = _input.Select(_ => new Vector3D { 0, 0, 0 }).ToList();
 
         var potential = _input.Select(_ => 0).ToList();
         var kinetic = _input.Select(_ => 0).ToList();
 
         for (var step = 0; step < numberOfSteps; step++)
         {
+            // Update velocities
             foreach (var pair in Enumerable.Range(0, 4).Subsets(subsetSize: 2))
             {
                 var first = pair[0];
@@ -55,46 +59,25 @@ public sealed class Day12 : BaseTestableDay
                 var firstPosition = positions[first];
                 var secondPosition = positions[second];
 
-                var diffs = new Vector3D(
-                    firstPosition.X.CompareTo(secondPosition.X),
-                    firstPosition.Y.CompareTo(secondPosition.Y),
-                    firstPosition.Z.CompareTo(secondPosition.Z)
-                );
+                var diffs = firstPosition.Zip(secondPosition).Select(t => t.First.CompareTo(t.Second)).ToList();
 
-                velocities[first] = new Vector3D(
-                    velocities[first].X - diffs.X,
-                    velocities[first].Y - diffs.Y,
-                    velocities[first].Z - diffs.Z
-                );
-
-                velocities[second] = new Vector3D(
-                    velocities[second].X + diffs.X,
-                    velocities[second].Y + diffs.Y,
-                    velocities[second].Z + diffs.Z
-                );
+                velocities[first] = velocities[first].Zip(diffs).Select(t => t.First - t.Second).ToList();
+                velocities[second] = velocities[second].Zip(diffs).Select(t => t.First + t.Second).ToList();
             }
 
+            // Update positions
             foreach (var moon in Enumerable.Range(0, 4))
             {
-                positions[moon] = new Vector3D(
-                    positions[moon].X + velocities[moon].X,
-                    positions[moon].Y + velocities[moon].Y,
-                    positions[moon].Z + velocities[moon].Z
-                );
+                positions[moon] = positions[moon].Zip(velocities[moon]).Select(t => t.First + t.Second).ToList();
             }
 
-            potential = positions.Select(m => Math.Abs(m.X) + Math.Abs(m.Y) + Math.Abs(m.Z)).ToList();
-            kinetic = velocities.Select(m => Math.Abs(m.X) + Math.Abs(m.Y) + Math.Abs(m.Z)).ToList();
+            potential = positions.Select(m => m.Sum(Math.Abs)).ToList();
+            kinetic = velocities.Select(m => m.Sum(Math.Abs)).ToList();
 
-            //Console.WriteLine($"After {step} steps");
-            //foreach (var moon in Enumerable.Range(0, 4))
-            //{
-            //    Console.WriteLine(
-            //        $"Moon {moon}: position ({positions[moon].X}, {positions[moon].Y}, {positions[moon].Z}) " +
-            //        $"with velocity ({velocities[moon].X}, {velocities[moon].Y}, {velocities[moon].Z}), " +
-            //        $"and energies potential={potential[moon]}, kinetic={kinetic[moon]}"
-            //    );
-            //}
+            if (firstPart)
+            {
+                continue;
+            }
         }
 
         return Enumerable.Range(0, 4).Select(moon => potential[moon] * kinetic[moon]).Sum();
@@ -102,12 +85,12 @@ public sealed class Day12 : BaseTestableDay
 
     private Answer CalculatePart1Answer()
     {
-        return SimulateGravity(1000);
+        return SimulateGravity(1000, true);
     }
 
     private Answer CalculatePart2Answer()
     {
-        return -1;
+        return SimulateGravity(100000, false);
     }
 
     public override ValueTask<string> Solve_1() => CalculatePart1Answer();
