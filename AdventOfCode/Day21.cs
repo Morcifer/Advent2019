b9aa37df90088ae83cca241b41adcdf5c6ed0831
Morcifer <mor.verbin@gmail.com>
2024-04-05 18:28:19 +0200

    Day 21: I hate programming in Assembly.

--- AdventOfCode/Day21.cs
+++ AdventOfCode/Day21.cs
@@ -20,25 +20,9 @@ public sealed class Day21 : BaseTestableDay
             .ToList();
     }
 
-    private Answer CalculatePart1Answer()
+    private Answer WalkOrRun(List<string> commands)
     {
         var computer = new Computer(_input);
-
-        // Whenever there is a hole followed by a ground (=true), jump to hit the ground (=false).
-        var commands = new List<string>
-        {
-            "NOT C T", // T is notC
-            "AND D T", // T is whether I should jump - if C is a hole and D is the ground
-            "OR T J", // Put that in J
-            "NOT B T", // T is notB
-            "AND C T", // T is whether I should jump - if B is a hole and C is the ground
-            "OR T J", // Put that in J
-            "NOT A T", // T is notA
-            "AND B T", // T is whether I should jump - if A is a hole and B is the ground
-            "OR T J", // Put that in J
-            "WALK",
-        };
-
         var commandIndex = 0;
 
         while (true)
@@ -66,9 +50,56 @@ public sealed class Day21 : BaseTestableDay
         }
     }
 
+    private Answer CalculatePart1Answer()
+    {
+        // Whenever there is a hole (= false) followed by a ground (= true), jump to hit the ground.
+        var commands = new List<string>
+        {
+            "NOT C T", // T is !C, so it's true when it's a hole
+            "AND D T", // T is whether I should jump - if C is a hole and D is ground
+            "OR T J", // Put that in J
+            "NOT B T", // T is !B, so it's true when it's a hole
+            "AND C T", // T is whether I should jump - if B is a hole and C is ground
+            "OR T J", // Put that in J
+            "NOT A T", // T is !A, so it's true when it's a hole
+            "AND B T", // T is whether I should jump - if A is a hole and B is ground
+            "OR T J", // Put that in J
+
+            "WALK",
+        };
+
+        return WalkOrRun(commands);
+    }
+
     private Answer CalculatePart2Answer()
     {
-        return -1;
+        // Whenever there is a hole followed by a ground (=true), jump to hit the ground.
+        // But make sure that where you land (D) is land, and it either lets you walk (E), or lets you jump (H).
+        var commands = new List<string>
+        {
+            "NOT C T", // Same as before
+            "AND D T",
+            "OR T J", 
+            "NOT B T",
+            "AND C T",
+            "OR T J", 
+            "NOT A T",
+            "AND B T",
+            "OR T J",
+
+            "OR J T", // Copy J into T: If J is true it goes into T. If it's false, it's because T was already false.
+
+            "AND D T", // Why did I not need this to be in the previous one? Because the input was built for it?
+            "AND D J",
+
+            "AND E T", // E is ground so I can walk to it.
+            "AND H J", // Or H should so I can jump there.
+            "OR T J",
+
+            "RUN",
+        };
+
+        return WalkOrRun(commands);
     }
 
     public override ValueTask<string> Solve_1() => CalculatePart1Answer();
