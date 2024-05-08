363f4f6686a222d0e18badad6e2054d83b3a9dea
Morcifer <mor.verbin@gmail.com>
2024-05-08 15:14:35 +0200

    Day 25: make a console game out of it, initial commit.

---
+++ AdventOfCode.Console/Program.cs
@@ -0,0 +1,67 @@
+﻿using AdventOfCode;
+
+var inputFilePath = "..\\..\\..\\..\\AdventOfCode\\Inputs\\25.txt";
+
+var program =  File
+    .ReadAllLines(inputFilePath)
+    .First()
+    .Split(',')
+    .Select(long.Parse)
+    .ToList();
+
+var computer = new Computer(program);
+
+var commands = new List<string>();
+var commandIndex = 0;
+
+Console.WriteLine("====================");
+Console.WriteLine("LET THE GAMES BEGIN!");
+Console.WriteLine("====================");
+
+while (true)
+{
+    var (returnMode, result) = computer.RunProgramToAsciiNewLine();
+
+    switch (returnMode)
+    {
+        case ReturnMode.Terminate:
+            Console.WriteLine(result);
+            return 0;
+        case ReturnMode.Output:
+            Console.WriteLine(result);
+            break;
+        case ReturnMode.Input:
+            var newCommand = Console.ReadLine();
+
+            if (newCommand is null)
+            {
+                Console.WriteLine("Invalid command, game over.");
+                return 0;
+            }
+
+            if (newCommand == "restart")
+            {
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+
+                Console.WriteLine("===============");
+                Console.WriteLine("RESTARTING GAME");
+                Console.WriteLine("===============");
+
+                computer = new Computer(program);
+
+                commands = new List<string>();
+                commandIndex = 0;
+                break;
+            }
+
+            commands.Add(newCommand);
+
+            var command = commands[commandIndex++];
+            computer.AddAsciiCommand(command);
+
+            break;
+    }
+}
+
